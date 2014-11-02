4aa94f692eda5c0cee4db8b0c92299fd30a61406
Nikolay Nemshilov <nemshilov@gmail.com>
2014-11-02 22:08:14 +1100

    Adding a simple grunt task to manage the compilation and dependencies

---
+++ gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          paths: ["./src"]
+        },
+        files: {
+          "./public/pain.less.css": "./src/pain.less"
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+        spawn: false
+      },
+      less: {
+        files: ["./src/*"],
+        tasks: ["less"],
+      },
+      html: {
+        files: ["./public/*"]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          base: "./public",
+          // keepalive: true,
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['connect', 'watch']);
+};
